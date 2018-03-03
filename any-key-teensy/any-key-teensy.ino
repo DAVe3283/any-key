f0f15ed92b8565671469f361201e09ac7f359ae2
Qwindelzorf <qwindelzorf@gmail.com>
2018-03-02 19:27:28 -0700

    Add initial support for LED and input pin debounce

--- any-key-teensy/any-key-teensy.ino
+++ any-key-teensy/any-key-teensy.ino
@@ -13,7 +13,8 @@
 // -----------------------------------------------------------------------------
 // Includes
 // -----------------------------------------------------------------------------
-
+#include <Bounce2.h>
+#include "FastLED.h"
 
 // -----------------------------------------------------------------------------
 // Constants
@@ -21,6 +22,10 @@
 
 // Pins
 const uint8_t led_pin(13);
+const uint8_t switch_pin(2);
+const uint8_t data_pin(7);
+const uint8_t clk_pin(14);
+const uint8_t num_leds(1);
 
 // -----------------------------------------------------------------------------
 // Declarations
@@ -30,7 +35,8 @@ const uint8_t led_pin(13);
 // -----------------------------------------------------------------------------
 // Globals
 // -----------------------------------------------------------------------------
-
+Bounce debouncer = Bounce();
+CRGB leds[num_leds];
 
 // -----------------------------------------------------------------------------
 // Function Definitions
@@ -41,10 +47,30 @@ void setup()
 {
   // Configure LED
   pinMode(led_pin, OUTPUT);
+  pinMode(switch_pin, INPUT_PULLUP);
+
+  debouncer.attach(switch_pin);
+  debouncer.interval(5); // interval in ms
+
+  FastLED.addLeds<APA102, data_pin, clk_pin>(leds, num_leds);
 }
 
 // Main program loop
 void loop()
 {
-  // Debounce inputs, do things, etc.
+  debouncer.update();
+  int value = debouncer.read();
+  if ( value == LOW ) {
+    digitalWrite(led_pin, HIGH );
+    leds[0] = CRGB::Red;
+  } 
+  else {
+    digitalWrite(led_pin, LOW );
+    leds[0] = CRGB::Blue;
+  }
+
+  FastLED.show();
+  //Keyboard.print("Hello World "); 
+  //Keyboard.println(count);
+  //delay(5000);
 }
